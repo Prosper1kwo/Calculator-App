63628f45e93e47464fcf9d5178e76219328a88d5
Prosper1kwo <89759268+Prosper1kwo@users.noreply.github.com>
2022-09-14 01:15:54 -0500

    Update to Final App
    Implemented the code to add functionality to the calculator application.

--- src/App.js
+++ src/App.js
@@ -6,20 +6,45 @@ function App() {
 
   const ops = ['/', '*', '+', '-', '.'];
 
-  
+  const updateCalc = value => {
+    if (
+      ops.includes(value) && calc == '' ||
+      ops.includes(value) && ops.includes(calc.slice(-1))
+    ) {
+      return;
+    }
+
+    setCalc(calc + value);
+
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString());    
+    }
+  }
 
   const createDigits = () => {
     const digits = [];
     for (let i=1; i<10; i++) {
       digits.push(
-        <button  key={i}>{i}</button>
+        <button onClick={() => updateCalc(i.toString())} key={i}>{i}</button>
       )
     }
 
     return digits;
   }
 
-  
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  }
+
+  const deleteLast = () => {
+    if (calc == '') {
+      return;
+    }
+    const value = calc.slice(0, -1);
+
+    setCalc(value);
+  }
+
   return (
     <div className="fi-App">
       <div className="fi-calculator">
@@ -28,20 +53,20 @@ function App() {
           {calc || "0"}
         </div>
         <div className="fi-operators">
-          <button >/</button>
-          <button >*</button>
-          <button >+</button>
-          <button >-</button>
+          <button onClick={() => updateCalc('/')}>/</button>
+          <button onClick={() => updateCalc('*')}>*</button>
+          <button onClick={() => updateCalc('+')}>+</button>
+          <button onClick={() => updateCalc('-')}>-</button>
 
-          <button >DEL</button>
+          <button onClick={deleteLast}>DEL</button>
         </div>
 
         <div className="fi-digits">
           {createDigits()}
-          <button >0</button>
-          <button >.</button>
+          <button onClick={() => updateCalc('0')}>0</button>
+          <button onClick={() => updateCalc('.')}>.</button>
           
-          <button >=</button>
+          <button onClick={calculate}>=</button>
         </div>
       </div>
     </div>
